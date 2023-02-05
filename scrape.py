315b95c0528a0a9ddfd20327921123aa92e95345
Ege Demir <62503047+demegire@users.noreply.github.com>
2023-02-05 16:27:24 +0300

    improve scraper

--- scrape.py
+++ scrape.py
@@ -1,17 +1,27 @@
 import eksipy
 import asyncio
+import csv
 import os
 
 
-async def getTopic():
+async def getTopic(topic):
     eksi = eksipy.Eksi()
-    topic = await eksi.getTopic("php")
-    entrys = await topic.getEntrys()
-    for entry in entrys:
-        print("*" * 10)
-        print(entry.text())
-        print(entry.author.nick)
-        print("*" * 10)
+    topic = await eksi.getTopic(topic)
+    entries = await topic.getEntrys()
+    
+    with open('{}.csv'.format(topic), mode='w', encoding='utf-8') as topic_csv:
+        writer = csv.writer(topic_csv, delimiter=',')
+        for entry in entries:
+            writer.writerow([entry.text()])
+            
+    #with open('{}.csv'.format(topic), mode='r', encoding='utf-8') as topic_csv:
+    #    csv_reader = csv.reader(topic_csv, delimiter=',')
+    #    for row in csv_reader:
+    #        print(row)
+
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(getTopic())
+loop.run_until_complete(getTopic('php'))
+
+
+
