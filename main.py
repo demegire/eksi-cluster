efabea282f12e987ff157b47a4fb304a339239a7
alpsertel <alparslansertel17@gmail.com>
2023-02-05 23:11:39 +0300

    embedder & main updated
    -clustering added for main
    -embedder reviewed

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import asyncio
 import pandas as pd
+import numpy as np
+import openai
 
 from scrape import get_topic
 from translate import translate
+from embedder import embedderEksi
+from sklearn.cluster import KMeans
+
+
 
 async def main():
     
@@ -13,6 +19,22 @@ async def main():
     for i in range(len(df)):
         df.iloc[i, 1] = translate(df.iloc[i,0])
     print(df)
+    df = embedderEksi(df)
+    matrix = np.vstack(df.embedding.values)
+    matrix.shape
+    
+    # clustering
+    
+    n_clusters = 2
+
+    kmeans = KMeans(n_clusters=n_clusters, init="k-means++", random_state=42)
+    kmeans.fit(matrix)
+    labels = kmeans.labels_
+    df["Cluster"] = labels
+    
+    print(df['Cluster'])
+
+    
     return df
 
 loop = asyncio.get_event_loop()
