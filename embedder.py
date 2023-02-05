932c3ff0a0b5b83bffb5f69962837371ff47fff7
alpsertel <48011901+alpsertel@users.noreply.github.com>
2023-02-05 17:21:05 +0300

    embedder
    v0.1 embedder added

---
+++ embedder.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import tiktoken
+import openai
+import os
+import numpy as np
+from openai.embeddings_utils import get_embedding
+
+def embedderEksi(df, column_to_embed):
+    # keys
+    openai.organization = ""
+    openai.api_key = ""
+    
+    # embedding model parameters
+    embedding_model = "text-embedding-ada-002"
+    embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
+    max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
+    
+    # embedding 
+    df["embedding"] = df.column_to_embed.apply(lambda x: get_embedding(x, engine=embedding_model))
+
+    return df
