1fbfc2fae48680db4bef648edf9cbce2d98c2c71
alpsertel <48011901+alpsertel@users.noreply.github.com>
2023-02-05 17:57:30 +0300

    main updated
    df format added

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import asyncio
+import pandas as pd
 
 from scrape import get_topic
 from translate import translate
 
 async def main():
-
-	#loop = asyncio.get_event_loop()
-	odtu = await get_topic('odtu')
-	print(translate(odtu[0]))
-	
-	
+    
+    #loop = asyncio.get_event_loop()
+    odtu = await get_topic('odtu')
+    df = pd.DataFrame(odtu, columns = ['entries'] )
+    df['translation'] = df['entries']
+    for i in range(len(df)):
+        df.iloc[i, 1] = translate(df.iloc[i,0])
+    print(df)
+    return df
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
