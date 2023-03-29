b188b954828dbdcb0c0ce3af24329e55c9fd7b27
kosedogus <kose.dogus@metu.edu.tr>
2023-03-29 21:12:32 +0300

    add test and minor changes in main &  scrape

--- scrape.py
+++ scrape.py
@@ -1,33 +1,25 @@
 import eksipy
 import asyncio
-import csv
-import os
+import pandas as pd
 
-
-async def write_topic(topic):
-    ''' Scrape the entries of given topic and saves them in a csv file '''
+async def write_topic(topic,start_page,end_page):
     eksi = eksipy.Eksi()
     topic = await eksi.getTopic(topic)
-    entries = await topic.getEntrys()
-    
-    with open('data\\{}.csv'.format(topic), mode='w', encoding='utf-8') as topic_csv:
-        writer = csv.writer(topic_csv, delimiter=',')
+    df = pd.DataFrame(columns=['entry'])
+    for page in range(start_page,end_page+1):
+        entries = await topic.getEntrys(page)
         for entry in entries:
-            writer.writerow([entry.text()])
-            
-async def get_topic(topic):
-    ''' Scrape the entries of the given topic and return a list of entry strings '''
+            df = df.append({'entry':entry.text()},ignore_index=True)
+    df.to_csv('data/{}.csv'.format(topic),index=False,encoding='utf-8')
+
+async def get_topic(topic,start_page,end_page):
     eksi = eksipy.Eksi()
     topic = await eksi.getTopic(topic)
-    entries = await topic.getEntrys()
-    
-    return [entry.text() for entry in entries]
+    df = pd.DataFrame(columns=['entry'])
+    for page in range(start_page,end_page+1):
+        entries = await topic.getEntrys(page)
+        for entry in entries:
+            df = df.append({'entry':entry.text()},ignore_index=True)
+    return df
 
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(getTopic('php'))
-
-def read_topic(topic):
-    ''' Read a csv file of topics and return a list of entry strings '''
-    with open('data\\{}.csv'.format(topic), mode='r', encoding='utf-8') as topic_csv:
-        csv_reader = csv.reader(topic_csv, delimiter=',')
-        return [row for row in csv_reader]
+# asyncio.run(write_topic("php",1,10))
